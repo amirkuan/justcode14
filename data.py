7a1752b83c662db9dbc6886615059f6686a38b71
amirkuan <amirkuanysbekov58@gmail.com>
2023-11-24 19:55:54 +0300

    Aidar

--- data.py
+++ data.py
@@ -3,5 +3,13 @@ import os
 
 df = pd.read_csv('people.csv')
 
-darina = df[['Name', 'Age']]
-print(darina)
+files = {'darina': 'Aidar.csv'}
+
+dar = df[['Name', 'Age']]
+
+if not os.path.exists('ata'):
+    os.makedirs('ata')
+
+dar.to_csv(os.path.join('ata.csv'), index=False)
+
+
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ files = {'Male': 'male_data.csv', 'Female': 'female_data.csv'}
 
 for key, value in files.items():
     filtered_df = df[df['Gender'] == key]
-    print(filtered_df)
+
     if not os.path.exists('filtered_data'):
         os.makedirs('filtered_data')
 
